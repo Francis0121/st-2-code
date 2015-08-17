669cfcbae256ee7fef032109690ef61aa23be17b
Francis <tjdrms0121@gmail.com>
2015-08-17 10:59:02 +0900

    가상 함수테이블의 존재한다 ...

--- 1일차/1_this_call8.cpp
+++ 1일차/1_this_call8.cpp
@@ -15,9 +15,21 @@ public :
 	}
 };
 
+// 100 Base::foo
+
+// 200 Dervied::foo
+
 int main() {
+
+
 	void(Base::*f)() = &Base::foo; // 잘 생각해보세요
 	
-	Base* p = new Base;
-	(p->*f)();// ?
+	// Base* p = new Base;
+	// (p->*f)(); // ? -> Base foo
+
+	cout << f << endl; // 가상함수의 주소는 가상함수 테이블의 인덱스가 들어온다.
+
+	Base* p = new Derived;
+	(p->*f)(); // ? -> Dervied foo
+
 }
