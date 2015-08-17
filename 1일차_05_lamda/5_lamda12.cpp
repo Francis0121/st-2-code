d34bb5a4f2a3276852aeb1ad12c58bbd7a1807d8
Francis <tjdrms0121@gmail.com>
2015-08-17 16:14:35 +0900

    멤버 변수 캡쳐

---
+++ 1일차_05_lamda/5_lamda12.cpp
@@ -0,0 +1,24 @@
+// 5_lamda12
+// ¸â¹ö º¯¼ö Ä¸ÃÄ
+
+#include <iostream>
+using namespace std;
+
+class Test {
+	int base;
+
+public:
+	Test(int n) : base(100) {
+
+	}
+
+	void foo() {
+		// ¸â¹ö º¯¼öÀÎ base¸¦ Ä¸ÃÄ ÇÏ·Á¸é?
+		auto f = [base]() { cout << base << endl; };
+	}
+};
+
+int main() {
+	Test t(20);
+	t.foo();
+}
