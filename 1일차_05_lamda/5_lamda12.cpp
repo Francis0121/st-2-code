fdd1c8c39643e88b33be6c604b1288c4aa997620
Francis <tjdrms0121@gmail.com>
2015-08-17 16:22:02 +0900

    Nullary Lamda expression

--- 1일차_05_lamda/5_lamda12.cpp
+++ 1일차_05_lamda/5_lamda12.cpp
@@ -17,6 +17,7 @@ public:
 		// X auto f = [base]() { cout << base << endl; };
 
 		// 참조 캡쳐는 아니지만 포인터를 값으로 했으므로 변경가능.
+		// 숙제1. 아래 코드를 사용했을 때 컴파일러가 만들어내는 클래스를 예측해 보세요
 		auto f = [this]() { base = 200; cout << base << endl; };
 		f();
 	}
---
+++ 1일차_05_lamda/5_lamda13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// auto f1 = []() { cout << "f1" << endl;  };
+	// = equal statement
+
+	// 인자가 없는 람다는 ()의 생략이 가능하다.
+	// Nullary Lamda expression
+	auto f1 = [] { cout << "f1" << endl;  };
+
+	f1();
+}
