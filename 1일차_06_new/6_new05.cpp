4b1704b5744f07a013bacc98f161bf57b964001f
Francis <tjdrms0121@gmail.com>
2015-08-17 17:39:45 +0900

    vector 에 사용자 타입을 저장하는 경우

--- 1일차_06_new/6_new05.cpp
+++ 1일차_06_new/6_new05.cpp
@@ -25,9 +25,27 @@ int main() {
 	cout << v.size() << endl; // 8								| size는 증가하고 메모리 개수는 계속 유지된다.
 	cout << v.capacity() << endl; // 실제 메모리 크기 10
 
-	v.shrink_to_fit(); // 필요 없는 공간을 제거해달라.
+	v.shrink_to_fit(); // 필요 없는 공간을 제거해달라. ---> CASE 02.를 실제로 부른다.( 할당 및 복사를 한다.)
 
 	cout << v.size() << endl; // 8								
 	cout << v.capacity() << endl; // 실제 메모리 크기 10
+
+	// 커지는 경우
+	vector<int> v2(10, 0);
+	v2.push_back(1); // 끝에 하나 추가
+					// 이 경우 기존 크기 * 1.5를 주소 사용
+					// 컴파일러에 따라 다를 수 있다.
+	cout << v2.capacity() << endl; // 실제 메모리 크기 10
+
+	// vector 에 사용자 타입을 저장하는 경우를 생각해 봅시다.
+	class DBConnect {};
+	vector<DBConnect> v3(10); // DBConnect는 생성자에서 DB에 접속합니다.
+	v3.resize(7); // 줄어든 3개의 객체는 메모리에 분명 남아 있습니다.
+				// 하지만 소멸자를 호출해서 DB를 닫아야 합니다.
+				// 소멸자의 명시적 호출이 필요 합니다.
+
+	v3.resize(8); // 늘어난 한개의 객체는 메모리에 이미 있습니다.
+				// 하지만 생성자를 호출해서 DB에 접속해야 합니다.
+				// 생성자의 명시적 호출이 필요합니다.
 
 }
