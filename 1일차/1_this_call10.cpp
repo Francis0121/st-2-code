b6ed15eb46f0ec9ac92fe0faa6611a0104ea2074
Francis <tjdrms0121@gmail.com>
2015-08-17 11:30:52 +0900

    C++11 함수의 추가 funcational 함수 (함수, 객체 포인터를 같이 쓸 수 있도록 하는 것? 모든 함수의 주소를 담는다)

---
+++ 1일차/1_this_call10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// 모든 함수의 주소를 담을 수 있는 도구
+// C, C++		: 문법적으로는 없다!
+// C#			: delegate 라는 문법
+// Objective-C	: Selector 라는 문법
+
+// C++11 : fucntion<> 모든 함수의 주소를 담을 수 있다.
+
+void foo() {
+	cout << "foo" << endl;
+}
+
+void goo(int a) {
+	cout << "goo  : " << a << endl;
+}
+
+void hoo(int a, int b) {
+	cout << "hoo : " << a << b << endl;
+}
+
+#include <functional>
+
+int main() {
+	function<void()> f = &foo;
+	f(); // ok .. foo()호출
+
+	//f = &goo;
+	//f();
+
+	//f = &hoo;
+	//f();
+
+	//f = &Dialog::Close();
+	//f();
+}
