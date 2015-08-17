e3312a8aa2ea885a47025ec4f747e500cce0628a
Francis <tjdrms0121@gmail.com>
2015-08-17 10:23:21 +0900

    this를 CreatThread시 파라미터로 넘겨서 해당 함수를 thread 내부에서 this를 이용하여 실행할 수있도록 한다.

--- 1일차/1_this_call4.cpp
+++ 1일차/1_this_call4.cpp
@@ -11,13 +11,20 @@ class Thread {
 public : 
 	void Create() {
 		// CreateThread 인 함수는 반드시 parameter 가 하나여야한다.
-		CreateThread(0, 0, _threadMain, 0, 0, 0); // !! Error
+		// this 를 넘겨주지 않는 경우
+		// CreateThread(0, 0, _threadMain, 0, 0, 0); // !! Error
+		// -> this 를 파라미터로 넘겨준다.
+		CreateThread(0, 0, _threadMain, this, 0, 0); // !! Error
 	}
 
 	// 스레드 함수
 	// 핵심 1. C의 callback 함수는 객체 지향으로 디자인 될때 static 멤버함수가 되어야한다.
 	static DWORD __stdcall _threadMain(void *p) {
-		threadMain();	// compile -> this->threadMain()
+		// 결국 p가 this임으로 캐스팅해서 사용합니다.
+		Thread* self = static_cast<Thread*>(p);
+		self->threadMain(); // 결국 threadMain(self)
+
+		// threadMain();	// compile -> this->threadMain()
 						// 즉 threadMain(this)가 되어야 한다.
 		return 0;
 	}
