5e145f004ddde7547712289e889e065baf407b20
Francis <tjdrms0121@gmail.com>
2015-08-17 15:26:31 +0900

    모든 함수객체는 다른 타입이다.

--- 1일차_05_lamda/5_lamda7.cpp
+++ 1일차_05_lamda/5_lamda7.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main() {
 	// 람다는 auto에 담을수 있습니다.
 	auto f1 = [](int a, int b) {return a + b; };
+	auto f2 = [](int a, int b) {return a + b; };
+
+	// f1과 f2는 같은 타입일까요? 다른 타입일까요?
+	// 모든 함수객체는 다른 타입이다.
+
+	// RTTI 기술로 컴파일러가 만들어준 함수 객체 클래스의 이름도 확인 가능합니다.
+	cout << typeid(f1).name() << endl;
+	cout << typeid(f2).name() << endl;
 
 	cout << f1(1, 2) << endl;
 }
