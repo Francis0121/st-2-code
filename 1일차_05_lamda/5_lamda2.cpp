2a93cd1e8e876897c5c77f285c707806c27adfdb
Francis <tjdrms0121@gmail.com>
2015-08-17 14:28:44 +0900

    if(cmp(x[i], x[j])) compare 함수의 분리

--- 1일차_05_lamda/5_lamda2.cpp
+++ 1일차_05_lamda/5_lamda2.cpp
@@ -10,7 +10,8 @@ using namespace std;
 void Sort(int* x, int n, bool(*cmp)(int, int)) {
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = j + 1; j < n; j++) {
-			if (x[i] > x[j])
+			// if (x[i] > x[j])
+			if(cmp(x[i], x[j]))
 				swap(x[i], x[j]);
 		}
 	}
