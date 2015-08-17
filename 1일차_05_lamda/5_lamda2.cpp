9a32d96736ca71291a9ba746decaa869b37bb412
Francis <tjdrms0121@gmail.com>
2015-08-17 14:27:46 +0900

    S/W 설게의 기본 원칙 : 변하지 않은 전체 알고리즘에서 변해야 하는 부분을 찾아서 분리한다.

--- 1일차_05_lamda/5_lamda2.cpp
+++ 1일차_05_lamda/5_lamda2.cpp
@@ -3,7 +3,11 @@
 #include <algorithm>
 using namespace std;
 
-void Sort(int *x, int n) {
+// S/W 설게의 기본 원칙 : 변하지 않은 전체 알고리즘에서 변해야 하는 부분을 찾아서 분리한다.
+
+// 일반 함수는 변하는 부분을 함수 인자화 한다.(함수 포인터)
+
+void Sort(int* x, int n, bool(*cmp)(int, int)) {
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = j + 1; j < n; j++) {
 			if (x[i] > x[j])
