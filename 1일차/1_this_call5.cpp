aa2a5d3806bde668787930f0d597a7f2d542acfb
Francis <tjdrms0121@gmail.com>
2015-08-17 10:31:39 +0900

    f1,f2,f3 함수에 대한 콜

--- 1일차/1_this_call5.cpp
+++ 1일차/1_this_call5.cpp
@@ -16,11 +16,16 @@ public:
 	}
 	int f3() {
 		cout << "f3" << endl;
-		return data;
+		return data; // compile -> this->data;
 	}
 };
 
 int main() {
 	Test *p = 0; // 메모리 할당에 실패 해서 0이 나왔다고 가정합니다.
 	p->f1(); // 어떻게 될까요? 실행하지말고 생각해보세요.
+			// f1(p), f1(0);
+	p->f2(); // ? ok
+			// f2(p), f2(0);
+	p->f3(); // ? "f3"까지 찍고 data의 멤버 변수가 this 콜이 되지 않기 때문에 프로그램이 죽는다
+			// f3(p), f3(0); 
 }
