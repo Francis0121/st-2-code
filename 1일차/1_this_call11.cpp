bee305c032af928005e34347903dfb046ca5cf01
Francis <tjdrms0121@gmail.com>
2015-08-17 11:45:27 +0900

    bind 사용 방법 인자..

--- 1일차/1_this_call11.cpp
+++ 1일차/1_this_call11.cpp
@@ -29,6 +29,10 @@ void hoo(int a, int b) {
 	cout << "hoo : " << a << ", " << b << endl;
 }
 
+void koo(int a, int b, int c, int d) {
+	printf("%d %d %d %d\n", a, b, c, d);
+}
+
 // fuction<ReturnType(Parameter)>
 #include <functional>
 using namespace std::placeholders; // _1, _2 .. 사용하기 위해 사용하는 것.
@@ -40,4 +44,12 @@ int main() {
 	// bind 를 사용하는 방법 인자 개수 만큼 반드시 넣어주어야 한다. 
 	f = bind(&hoo, _1, 3); // hoo(5, 3);
 	f(5); 
+
+	// _1, _2 는 내가 보내는 f2()에서의 파라미터 순서이다.
+	function<void(int, int)> f2;
+	f2 = bind(&koo, _2, 2, 9, _1);
+	f2(6, 3); // 3 2 9 6 
+
 }
+
+// Placeholders _1, _2, _3; 로 객체 형식 이다.. 가변인자 템플릿이라는 것도 들어간다.
