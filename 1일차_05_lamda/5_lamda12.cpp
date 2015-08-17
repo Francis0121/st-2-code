63d83112336618554bcd0093527658008abbc9e9
Francis <tjdrms0121@gmail.com>
2015-08-17 16:18:14 +0900

    // 참조 캡쳐는 아니지만 포인터를 값으로 했으므로 변경가능.

--- 1일차_05_lamda/5_lamda12.cpp
+++ 1일차_05_lamda/5_lamda12.cpp
@@ -8,14 +8,16 @@ class Test {
 	int base;
 
 public:
-	Test(int n) : base(100) {
+	Test(int n) : base(n) {
 
 	}
 
 	void foo() {
 		// 멤버 변수인 base를 캡쳐 하려면?
 		// X auto f = [base]() { cout << base << endl; };
-		auto f = [this]() { cout << base << endl; };
+
+		// 참조 캡쳐는 아니지만 포인터를 값으로 했으므로 변경가능.
+		auto f = [this]() { base = 200; cout << base << endl; };
 		f();
 	}
 };
