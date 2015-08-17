7ca4db301d6aeba23d3442bfc27b98e271a185ab
Francis <tjdrms0121@gmail.com>
2015-08-17 16:46:37 +0900

    Function overloading....

--- 1일차_06_new/6_new02.cpp
+++ 1일차_06_new/6_new02.cpp
@@ -8,6 +8,12 @@ void* operator new(size_t sz){
 	return malloc(sz);
 }
 
+// Function Overloading이 가능하다. 단, 1번째 인자는 반드시 size_t이어야 한다.
+void* operator new(size_t sz, char* s, int n){
+	cout << "my new 2" << endl;
+	return malloc(sz);
+}
+
 void operator delete(void *p) {
 	cout << "my delete" << endl;
 	free(p);
@@ -15,5 +21,8 @@ void operator delete(void *p) {
 
 int main() {
 	int *p1 = new int; // sizeof(int) 즉 "4"가 인자로 전달됨.
+	int *p2 = new("AAA", 3) int; //Funcation Overloading operator new 인자로 넘기는 방법
+
 	delete p1;
+	delete p2;
 }
