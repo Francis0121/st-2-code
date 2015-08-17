b3f102b42c29efbceb697543c65a64df816fbc21
Francis <tjdrms0121@gmail.com>
2015-08-17 09:45:21 +0900

    1_this_call 이 어려움

---
+++ 1일차/1_this_call3.cpp
@@ -0,0 +1,16 @@
+// this_call 관리의 어려움
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+using namespace std;
+
+// this 관리의 어려움!
+DWORD __stdcall foo(void *p) {
+	cout << "foo" << endl;
+	return 0;
+}
+
+int main() {
+	CreateThread(0, 0, foo, "A", 0 0); // 스레드를 만드는 함수
+	_getch();
+}
