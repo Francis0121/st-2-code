0870d545057bf7f42cf03ff0ac91a61893d3ac87
Francis <tjdrms0121@gmail.com>
2015-08-17 09:36:49 +0900

    멤버 변수 함수 포인터 작성하는 방법 계속 error 작성중

--- 1일차/1_this_call2.cpp
+++ 1일차/1_this_call2.cpp
@@ -20,8 +20,16 @@ void foo(){
 int main() {
 	void(*f1)() = &foo; // ok!! 함수포인터입니다.
 	// void(*f2)() = &Dialog::Close;// 될까요? 
+				// error.
 
 	// 멤버 함수 포인터를 만드는 법
 	void(Dialog::*f3)() = &Dialog::Close; // ok .. 외워두세요.
-	f3();
+	// f3(); // 될까요 ? 
+		// error. this 가 없다.
+	Dialog dlg;
+	// dlg.f3(); // ok 결국 dlg.Close() 즉 f3(&dlg);
+			// 그런데 이 경우 컴파일러는 f3이라는 멤버를 찾게된다.
+			// 그래서 Error.
+
+	dlg.*f3(); // f3는 함수 포인터이므로 *f3하면 함수가 된다.
 }
