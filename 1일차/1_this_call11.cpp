f7577e463acdba3bf05a650d75791f6f0499a87c
Francis <tjdrms0121@gmail.com>
2015-08-17 11:39:41 +0900

    bind 함수의 정확한 사용법 인자의 개수만큼 이용해야 한다.

---
+++ 1일차/1_this_call11.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// 모든 함수의 주소를 담을 수 있는 도구
+// C, C++		: 문법적으로는 없다!
+// C#			: delegate 라는 문법
+// Objective-C	: Selector 라는 문법
+
+// C++11 : fucntion<> 모든 함수의 주소를 담을 수 있다.
+
+class Dialog {
+
+public:
+	void Close() {
+		cout << "Dialog Close" << endl;
+	}
+
+};
+
+void foo() {
+	cout << "foo" << endl;
+}
+
+void goo(int a) {
+	cout << "goo  : " << a << endl;
+}
+
+void hoo(int a, int b) {
+	cout << "hoo : " << a << ", " << b << endl;
+}
+
+// fuction<ReturnType(Parameter)>
+#include <functional>
+using namespace std::placeholders; // _1, _2 .. 사용하기 위해 사용하는 것.
+int main() {
+	function<void(int)> f = &goo;
+	f(5); // goo(5)
+
+	// Parameter 첫번째 인지 두번째 인지 알 수 없다.
+	// bind 를 사용하는 방법 인자 개수 만큼 반드시 넣어주어야 한다. 
+	f = bind(&hoo, _1, 3); // hoo(5, 3);
+	f(5); 
+}
