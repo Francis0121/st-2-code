4ba8811f0a748961b5112882cff9c6d5e7f8e6d7
Francis <tjdrms0121@gmail.com>
2015-08-17 15:33:43 +0900

    inline 함수로 치환되는 것은 auto만!

--- 1일차_05_lamda/5_lamda8.cpp
+++ 1일차_05_lamda/5_lamda8.cpp
@@ -7,14 +7,17 @@ int main() {
 
 	// auto
 	auto f1 = [](int a, int b) {return a + b; };
+	// 함수 객체가 결정 됨으로 inline 치환 될 수 있다.
 	
 	// 함수 포인터
 	int(*f2)(int,int) = [](int a, int b) {return a + b; };
+	// inline 치환될 수 없다. 함수포인터로 다 담을 수 있다.
 
 	// C++11 fuction 
 	function<int(int, int)> f3 = [](int a, int b) {return a + b; };
+	// inline 치환될 수 없다. binde로 다 담을 수 있다.
 
 	f1(1, 2);
 	f2(1, 2);
-	f3(1, 2);
+	f3(1, 2); 
 }
