f89b37a8901ded1c697a0f8473dc043987a79758
Francis <tjdrms0121@gmail.com>
2015-08-17 12:23:09 +0900

    상수  함수 toString 의 문제점 해결방법은 4에

--- 1일차_상수함수/2_상수함수3.cpp
+++ 1일차_상수함수/2_상수함수3.cpp
@@ -4,20 +4,26 @@ using namespace std;
 // 논리적 상수성
 class Point {
 	int x, y;
+	char cache[32];
+	bool cache_valid;
+
 public:
-	Point(int a = 0, int b = 0) : x(a), y(b) {}
+	Point(int a = 0, int b = 0) : x(a), y(b), cache_valid(false){}
 
 	// 객체의 상태를 문자열로 반환하는 함수 : java, c#에 있는 개념
-	char* toString() {
-		static char s[32];
-		sprintf(s, "%d, %d", x, y);
-		return s;
+	char* toString()  const {
+		// static char s[32];
+		if (cache_valid == false) {
+			sprintf_s(cache, "%d, %d", x, y);
+			cache_valid = true;
+		}
+		return cache;
 	}
 
 };
 
 int main() {
 	Point p(1, 2);
-	cout << p.toString << endl;
-	cout << p.toString << endl;
+	cout << p.toString() << endl;
+	cout << p.toString() << endl;
 }
