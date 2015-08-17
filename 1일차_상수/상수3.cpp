31a4f07921c74eb08becdf10afcfcf92a01cf6b3
Francis <tjdrms0121@gmail.com>
2015-08-17 14:00:58 +0900

    www.cppreference.com 에서 constexpr 을 검색

--- 1일차_상수/상수3.cpp
+++ 1일차_상수/상수3.cpp
@@ -21,6 +21,8 @@ int main() {
 	// constexpr int c4 = n; // error 컴파일 상수가 아니다.
 
 	// 배열 크기로 함수도 사용가능
-	// 단, 함수리턴값은 __ 이어야 한다.
+	// 단, 함수리턴값은 _constexpr_ 이어야 한다.
 	int x[foo(3, 4)];
 }
+
+// www.cppreference.com 에서 constexpr 을 검색
