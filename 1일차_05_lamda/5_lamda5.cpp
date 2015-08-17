c1eb1866dd9a8e0c24957a14edd8a84d8fde7015
Francis <tjdrms0121@gmail.com>
2015-08-17 15:14:18 +0900

    2. 비교정책으로 함수 객체를 사용하는 경우.  // 장점 : 정책이 인라인 치환 됨으로 속도가 빠르다  // 단점 : 정책을 교체한 횟수 만큼의 sort()함수 생성. 코드 메모리 증가.

--- 1일차_05_lamda/5_lamda5.cpp
+++ 1일차_05_lamda/5_lamda5.cpp
@@ -10,6 +10,18 @@ bool cmp2(int a, int b) {
 	return a > b;
 }
 
+struct less_cmp {
+	inline bool operator()(int a, int b) {
+		return a < b;
+	}
+};
+
+struct greater_cmp {
+	inline bool operator()(int a, int b) {
+		return a > b;
+	}
+};
+
 int main() {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
 
@@ -19,4 +31,12 @@ int main() {
 	// 단점 : 정책이 인라인 치환될 수 없기 때문에 성능 저하가 있다.
 	sort(x, x + 10, cmp1); // sort(int* ,int* , bool(*)(int,int)) 함수 생성
 	sort(x, x + 10, cmp2); // sort(int* ,int* , bool(*)(int,int)) 함수 생성
+
+	// 2. 비교정책으로 함수 객체를 사용하는 경우.
+	// 장점 : 정책이 인라인 치환 됨으로 속도가 빠르다
+	// 단점 : 정책을 교체한 횟수 만큼의 sort()함수 생성. 코드 메모리 증가.
+	less_cmp f1;
+	greater_cmp f2;
+	sort(x, x + 10, f1); // sort(int*, int*, less) 함수 생성
+	sort(x, x + 10, f2); // sort(int*, int*, greater) 함수 생겅
 }
