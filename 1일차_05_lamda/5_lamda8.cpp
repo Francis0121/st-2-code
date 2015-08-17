b1015240780ccee9cd2fe5ad1a9fddc093a41783
Francis <tjdrms0121@gmail.com>
2015-08-17 15:40:26 +0900

    ★ auto는 절대 함수 parameter가 될 수 없다. 인라인 치환 가능하게 람다를 받는 유일한 방법은 -template

--- 1일차_05_lamda/5_lamda8.cpp
+++ 1일차_05_lamda/5_lamda8.cpp
@@ -20,4 +20,24 @@ int main() {
 	f1(1, 2);
 	f2(1, 2);
 	f3(1, 2); 
+
+	// 람다와 함수 인자
+	foo([](int a, int b) { return a + b; });
+	// foo1( [] (int a, int b) { return a + b; } );
+	// foo2([](int a, int b) { return a + b; });
+}
+
+// 람다 인자를 받는 방법
+
+// ★ auto는 절대 함수 parameter가 될 수 없다.
+// void foo(auto f1){} // foo(1)	=> int type
+					// fof(3.4)	=> double
+
+// 아래 2개는 가능하지만 인라인 치환 되지 않는다.
+// void foo1(int(*f2)(int, int)) {} 
+// void foo2(function<int(int, int)> f3) {}
+
+// 인라인 치환 가능하게 람다를 받는 유일한 방법은 -template
+template<typename T> void foo(T f) {
+	f(1, 2);
 }
