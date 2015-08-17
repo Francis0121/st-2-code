d2c48520e3b2be5f69ec451f98806473c20893a0
Francis <tjdrms0121@gmail.com>
2015-08-17 17:49:10 +0900

    // #define new new(nothrow)

--- 1일차_06_new/6_new06.cpp
+++ 1일차_06_new/6_new06.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 // 아래 코드에서 잘못된 점을 찾아보세요.
 
-//int main() {
+// 1998 표준화 이전에 만든 코드가 있다면 아래 한 줄만 추가한다.
+// #define new new(nothrow) 
+
+// int main() {
 //	int *p = new int[100];
 //
 //	if (p == 0) {
@@ -17,6 +20,11 @@ using namespace std;
 
 // new 실패시 예외가 발생합니다.
 int main() {
+
+	int *p2 = new(nothrow) int[100]; // 실패시 0리턴
+	// 1998년 이전에는 할당 실패하면 0을 리턴한다.
+	// 1998년 이후 표준에는 exception을 반환한다.
+
 	int *p;
 	
 	try {
