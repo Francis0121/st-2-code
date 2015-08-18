0a3d7fd9bc9ad8ab4b373063da24fca328dbde4f
Francis <tjdrms0121@gmail.com>
2015-08-18 15:35:31 +0900

    primary template IsArray 만들기 ..머리 과부하

--- 2일차_08_traits/8_traits8.cpp
+++ 2일차_08_traits/8_traits8.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int a;
+int* p; // int* 가 타입이다.
+int x[10]; // int [10] 가 x의 타입이다.
+			// T[N]
+
 // primary template : fals_type을 기본 클래스로 
 template<typename T> struct IsArray : false_type{};
+template<typename T, int N> struct IsArray<T[N]> : true_type {};
 
 // traits를 만드는 방법 다시 정리
 template<typename T> void foo(const T& a) {
