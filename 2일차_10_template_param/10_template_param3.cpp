a67b11579d997e5e377dfcb74f30003da427c891
Francis <tjdrms0121@gmail.com>
2015-08-18 17:17:15 +0900

    상속에서 내부 멤버 변수로 변경한다.

--- 2일차_10_template_param/10_template_param3.cpp
+++ 2일차_10_template_param/10_template_param3.cpp
@@ -10,20 +10,37 @@ using namespace std;
 
 // Adapter 디자인 패턴 : 기존 클래스의 인터페이스 (함수 이름)을 변경해서 클라이언트가 요구하는 새로운 클래스를 만드는 패턴 
 //						C++은 템플릿과 인라인으로 성능저하없이 만들수도 있다.
-template<typename T> class stack : public list<T> {
+//template<typename T> class stack : public list<T> {
+//public:
+//	inline void push(cosnt T& a) {
+//		list<T>::push_back(a);
+//	}
+//	inline void pop() {
+//		list<T>::pop_back();
+//	}
+//	inline T& top() {
+//		return list<T>::back();
+//	}
+//};
+
+// S/W 재사용은 상속 vs 포함이 있다. 포함이 좋은 경우가 더 많다.
+template<typename T> class stack{
+	list<T> st;
 public:
 	inline void push(cosnt T& a) {
-		list<T>::push_back(a);
+		st.push_back(a);
 	}
 	inline void pop() {
-		list<T>::pop_back();
+		st.pop_back();
 	}
 	inline T& top() {
-		return list<T>::back();
+		return st.back();
 	}
 };
+
 
 int main() {
 	stack<int>	st;
 	st.push(10);
+	// st.push_front(20); // 사용자가 실 수 했다.
 }
