d08f0d198b023f1f9ba86c3f46413cac3d5f5a2f
Francis <tjdrms0121@gmail.com>
2015-08-18 11:09:31 +0900

    typename을 적어야 해당 것에 대한 것을 이해 시킬 수 있다.

--- 2일차_05_typename/5_typename1.cpp
+++ 2일차_05_typename/5_typename1.cpp
@@ -11,9 +11,11 @@ int p = 0;
 
 template<typename T> void foo(T a) {
 	// AAA::DWORD
-	T::DWORD * p; // 이 한줄을 해석해 보세요 
+	typename T::DWORD * p; // 이 한줄을 해석해 보세요 
 				// 1. DWORD 는 static 멤버 data이다. 곱하기 p를 하고 있다.
 				// 2. DWORD는 typedef 등으로 만든 타입이다. 포인터 변수 p를 선언하고 있다.
+
+	// typename 을 적어야지만 typedef를 판단 할 수 있다.! 
 }
 
 int main() {
