00c255e84ed6581470f6e8fe4c595d80ff012c72
Francis <tjdrms0121@gmail.com>
2015-08-18 15:39:37 +0900

    배열의 크기도 알 수 있다.

--- 2일차_08_traits/8_traits8.cpp
+++ 2일차_08_traits/8_traits8.cpp
@@ -7,13 +7,17 @@ int x[10]; // int [10] 가 x의 타입이다.
 			// T[N]
 
 // primary template : fals_type을 기본 클래스로 
-template<typename T> struct IsArray : false_type{};
-template<typename T, int N> struct IsArray<T[N]> : true_type {};
+template<typename T> struct IsArray : false_type{
+	static const int size = -1; // 배열이 아님으로
+};
+template<typename T, int N> struct IsArray<T[N]> : true_type {
+	static const int size = N;
+};
 
 // traits를 만드는 방법 다시 정리
 template<typename T> void foo(const T& a) {
 	if (IsArray<T>::value) {
-		cout << "배열" << endl;
+		cout << "배열, 크기는 " << " " << IsArray<T>::size << endl;
 	} else {
 		cout << "배열 아님" << endl;
 	}
