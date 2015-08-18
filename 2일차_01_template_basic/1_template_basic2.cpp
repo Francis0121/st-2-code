5b5a4256677f0c3b16a10190d1233f404c2ef352
Francis <tjdrms0121@gmail.com>
2015-08-18 09:25:05 +0900

    명시적 추론을 사용하면 메모리 코드양을 줄일 수가 있다.

--- 2일차_01_template_basic/1_template_basic2.cpp
+++ 2일차_01_template_basic/1_template_basic2.cpp
@@ -8,5 +8,8 @@ int main(){
 	short s = 3;
 
 	square(3); // int 버전
-	square(s); // short 버전
+	// square(s); // short 버전
+	square<int>(s);
 }
+
+// VC++ Command Prompt 실행 : cl 1_template_basic2.cpp /FAs
