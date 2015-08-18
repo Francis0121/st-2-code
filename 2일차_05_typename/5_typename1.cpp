ab45f1c766b6853116298adf863c1df96e1eb154
Francis <tjdrms0121@gmail.com>
2015-08-18 11:11:52 +0900

    템플릿 typename에 대한 이해

--- 2일차_05_typename/5_typename1.cpp
+++ 2일차_05_typename/5_typename1.cpp
@@ -9,6 +9,15 @@ public:
 
 int p = 0;
 
+// 템플릿 안에 있는 타입을 꺼낼 때는 반드시 typeanme을 적어야 합니다.
+
+template<typename T> typename T::DWORD a;
+
+// 템플릿이 아닐때는 사용하면 안됩니다.
+ 
+typename AAA::DWORD a; // error. AAA는 템플릿이 아니고 일반 타입입니다.
+
+// template<class T> -> 초기 밑에 이유때문에 typename을생성 그래서 여기도 typename으로 변경
 template<typename T> void foo(T a) {
 	// AAA::DWORD
 	typename T::DWORD * p; // 이 한줄을 해석해 보세요 
