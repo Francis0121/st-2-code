034dcd3a88837f45fe46db10bff4feaa657b9d78
Francis <tjdrms0121@gmail.com>
2015-08-18 09:08:12 +0900

    유사한 코드가 반복되면 코드생성기를 사용하자. 1. 매크로에 의한 코드 생성

--- 2일차_01_template_basic/1_template_baisc.cpp
+++ 2일차_01_template_basic/1_template_baisc.cpp
@@ -1,12 +1,23 @@
 // Template_basic
 
-int square(int a) {
-	return a * a;
-}
+// 함수 오버로딩
+// 함수 사용자 : 하나의 함수 처럼 보인다.
+// 함수 제작자 : 2개의 함수를 만들어야 한다.
 
-double square(double a) {
-	return a * a;
-}
+//int square(int a) {
+//	return a * a;
+//}
+//
+//double square(double a) {
+//	return a * a;
+//}
+
+// 유사한 코드가 반복되면 코드생성기를 사용하자.
+// 1. 매크로에 의한 코드 생성
+#define MAKE_SQUARE(T) T square(T a){return a * a;}
+
+MAKE_SQUARE(int)
+MAKE_SQUARE(double)
 
 int main() {
 	square(3);
