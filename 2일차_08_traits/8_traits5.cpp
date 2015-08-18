af3cc7735b3054495f0f2fb94547eb644970bd39
Francis <tjdrms0121@gmail.com>
2015-08-18 14:42:14 +0900

    int2type을 이용한 함수 오버로딩!!

---
+++ 2일차_08_traits/8_traits5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// 0, 1 로 함수 오버로딩이 되게 하는 기술
+template<int N> struct int2type {
+	enum { value = N };
+};
+// 모든 interger를 type으로 바꿔주는 역할을한다.
+
+//int main() {
+//	foo(0);
+//	foo(1); // 0,1은 같은 타입이다. foo(0), foo(1) 같은 함수 호출.
+//	int2type<0> t0;
+//	int2type<1> t1;
+//	foo(t0);
+//	foo(t1); // t0와 t1은 다른 타입이다. foo(t0), foo(t1)은 다른 함수 호출.
+//}
+
+
+template<typename T> struct IsPointer {
+	enum { value = false };
+};
+template<typename T> struct IsPointer<T*> {
+	enum { value = true };
+};
+
+// 포인터인 경우를 처리하는 함수
+template<typename T> void printv_imp(T a, int2type<1>) {
+	cout << a << " " << *a << endl;
+}
+
+// 포인터가 아닌 경우
+// 파라미터 변수이름을 안적게 되면 최적화시 안쓰는 값으로 판단하여 최적화를 알아서 해준다.
+template<typename T> void printv_imp(T a, int2type<0>) {
+	cout << a << endl;
+}
+
+template<typename T> void printv(T a) {
+	// 어떤 구조체나 클래스에 ()을 붙이면 임시객체를 생성해준다.
+	printv_imp(a, int2type<IsPointer<T>::value>() );
+}
+
+int main() {
+	int n = 3;
+	printv(&n);
+	printv(n);
+}
