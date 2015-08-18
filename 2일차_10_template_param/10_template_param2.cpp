dfa5b014e0708689454942f877b04cda7a977d8a
Francis <tjdrms0121@gmail.com>
2015-08-18 17:08:07 +0900

    10_template_param2

--- 2일차_10_template_param/10_template_param2.cpp
+++ 2일차_10_template_param/10_template_param2.cpp
@@ -1,9 +1,15 @@
 // 10 템플릿 인자 2
 
+// 3. template 인자로 template 을 보낼 수 있다.
+
+// = template<class T> class list
 template<typename T> class list {
 
 };
 
+// core/include/utils/List.h
+// 불가 template<typename T, template<typename> typename C> 
+// 가능 template<typename T, template<class> class C>  ---> 위에 템플릿 참고
 template<typename T, template<typename> class C> class stack {
 	//C c; // error. C는 template 이다.
 
