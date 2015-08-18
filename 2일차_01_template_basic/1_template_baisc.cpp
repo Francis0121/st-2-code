c64d21af4f8df013483d1a207052a058fd19fd86
Francis <tjdrms0121@gmail.com>
2015-08-18 09:18:08 +0900

    템플릿에 대한 개념 추가

--- 2일차_01_template_basic/1_template_baisc.cpp
+++ 2일차_01_template_basic/1_template_baisc.cpp
@@ -28,14 +28,26 @@
 //    어떤 타입의 함수가 필요한지를 미리 알려줄 필요가 없다.
 template<typename T> T square(T a){return a * a;}
 
+// template instantiation : T의타입을 결정해서 실제 함수를 만드는 과정
+
+// 암시적 instantiation : T의 타입을 컴파일러가 결정(추론, Deduction) 해서 함수 생성
+
+// 명시적 instantiation : T의 타입을 사용자가 전달해서 함수 생성
+
+// Code Bloat : 너무나 많은 함수/클래스가 생성되어 코드메모리가 증가하는것.
+
 int main() {
+
+	// ~ 암시적 추론
 	square(3); // compile -> int square(int a){return a * a;}
 	square(3.3); // compile -> double square(double a){return a * a;}
 
 	short s = 3;
 	square(s);	// 1. int 버전 사용 2. short 버전 생성.
 				// 2. short 버전을 생성해준다.
+				// -> Code Bloat 이다!!
 	
+	// ~ 명시적 추론
 	square<int>(s); // 사용자가 T 의 타입을 전달 - int 버전을 사용해 달라.
 
 }
