55e2843fd36303bda8c3d7821f12ac6a5cfc8f13
Francis <tjdrms0121@gmail.com>
2015-08-18 10:22:45 +0900

    Zero initialize : T a = T()

--- 2일차_04_member_func_template/4_member_func_template1.cpp
+++ 2일차_04_member_func_template/4_member_func_template1.cpp
@@ -5,12 +5,16 @@
 //	  double이면 될 것 같지만 double은 성능이 느립니다.
 
 
+// 2. Zero initialize : T a = T()
+//	  T가 표준 타입이거나 포인터 이면 0으로 초기화
+//	  T가 사용자 타입이면 Default 생성자로 초기화
+
 template<typename T> class complex {
 	T re;
 	T im;
 
 public:
-	complex(T r = 0, T i = 0) : re(r), im(i){ }
+	complex(T r = T(), T i = T()) : re(r), im(i){ }
 
 };
 
