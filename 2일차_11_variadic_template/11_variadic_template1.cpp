9613a33928d0b8b635c552c1a7b6d606ec2ba55b
Francis <tjdrms0121@gmail.com>
2015-08-18 17:48:15 +0900

    가변인자 가변인자....

--- 2일차_11_variadic_template/11_variadic_template1.cpp
+++ 2일차_11_variadic_template/11_variadic_template1.cpp
@@ -6,8 +6,25 @@ template<typename ... Types> void goo(Types ... args) {
 	cout << "goo" << endl;
 }
 
-template<typename ... Types> void foo(Types ... args) {
+void hoo(int a, double d, char c) {
+	cout << "hoo" << endl;
+}
 
+void zoo(int* a, double* b, char* c) {
+	cout << "zoo" << endl;
+}
+
+// Types : 여러개의 타입을 가지고 있는 타입의 집합
+// args : 여러개의 값을 가지고 있는 인자 집합 - " Parameter Pack"
+template<typename ... Types> void foo(Types ... args) { // ★ ... 위치 중요
+	// Parameter Pack 안에 인자 갯수를 알고 싶다면
+	cout << sizeof...(args) << endl;
+	cout << sizeof...(Types) << endl;
+
+	// Parameter Pack 을 다른 함수로 전달하는 방법
+	goo(args...); // ★ ... 위치 중요
+	hoo(args...);
+	zoo(&args...); // 각각 변수에 대한 주소값으로 넘어간다.
 }
 
 int main() {
