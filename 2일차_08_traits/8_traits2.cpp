510a8d0f8f1ec9290d6e8298e5dc4a91e05cc952
Francis <tjdrms0121@gmail.com>
2015-08-18 14:13:53 +0900

    정리

--- 2일차_08_traits/8_traits2.cpp
+++ 2일차_08_traits/8_traits2.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 // Traits 기술 : T의 다양한 속성(특징)을 컴파일 시간에 알아내는 기술
 
+// T에 대해서 조사할 수 있는 메타 함수
+// 만드는 방법
+// primary template : false 리턴 ( value = false 라는 의미 )
+// 부분 전문화 버전 : true 리턴 ( value = true 라는 의미 )
+
 template<typename T> struct IsPointer {
 	enum { value = false };
 };
