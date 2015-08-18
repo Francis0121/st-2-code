a8a504f02c76bae96e873038a3335b25a868a886
Francis <tjdrms0121@gmail.com>
2015-08-18 09:13:25 +0900

    2. 컴파일러에 의한 코드 생성

--- 2일차_01_template_basic/1_template_baisc.cpp
+++ 2일차_01_template_basic/1_template_baisc.cpp
@@ -13,13 +13,22 @@
 //}
 
 // 유사한 코드가 반복되면 코드생성기를 사용하자.
-// 1. 매크로에 의한 코드 생성
-#define MAKE_SQUARE(T) T square(T a){return a * a;}
+// 1. 매크로에 의한 코드 생성 - 전처리기가 코드 생성
+//    단점 : 전처리기는 타입을 알지 못한다.
+//			어떤 타입의 함수가 필요한지를 미리 선언해야 한다.
 
-MAKE_SQUARE(int)
-MAKE_SQUARE(double)
+//#define MAKE_SQUARE(T) T square(T a){return a * a;}
+//
+//MAKE_SQUARE(int)
+//MAKE_SQUARE(double)
+
+
+// 2. 컴파일러에 의한 코드 생성
+//    컴파일러는 컴파일 중에 타입을 알 수 있다.
+//    어떤 타입의 함수가 필요한지를 미리 알려줄 필요가 없다.
+template<typename T> T square(T a){return a * a;}
 
 int main() {
-	square(3);
-	square(3.3);
+	square(3); // compile -> int square(int a){return a * a;}
+	square(3.3); // compile -> double square(double a){return a * a;}
 }
