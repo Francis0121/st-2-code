b8717ae7f356a77e05b06edfe470e1c582cc144e
Francis <tjdrms0121@gmail.com>
2015-08-19 09:26:43 +0900

    가변인자 템플릿 완성못함 ㅠㅠ

--- 3일차_01_variadic_template/1_variadic_template2.cpp
+++ 3일차_01_variadic_template/1_variadic_template2.cpp
@@ -1,3 +1,4 @@
+//~ 아직 부족
 #include <iostream>
 using namespace std;
 
@@ -18,6 +19,29 @@ public:
 	};
 };
 
+template<typename T, typename ... Types> 
+class xtuple : public xtuple<Types...> {
+public:
+	T value;
+
+	xtuple() {
+
+	}
+
+	xtuple(const T& a, Types ... args) : value(a), xtuple<Types...>(args...) {}
+
+	enum {
+		N = 1 + xtuple<Types...>::N;
+	};
+};
+
+template<typename T> int xtuple_size(T& a) {
+	return T::N;
+}
+
+
 int main() {
+	xtuple<int, char, double, short> t4(1, 'c', 3.3, 4);
+	cout << xtuple_size(t4) << endl;
 	xtuple<int> t1(3);
 }
