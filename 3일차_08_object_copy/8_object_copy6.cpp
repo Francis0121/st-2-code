5e9fa103ed927043ea98cb4d95b56c88d46e8eea
Francis <tjdrms0121@gmail.com>
2015-08-19 16:43:19 +0900

    move statement 정리

---
+++ 3일차_08_object_copy/8_object_copy6.cpp
@@ -0,0 +1,39 @@
+// 8_객체복사6
+#include <iostream>
+using namespace std;
+
+class Cat {
+public:
+	Cat() {
+		cout << "생성자" << endl;
+	}
+	~Cat() {
+		cout << "소멸자" << endl;
+	}
+	Cat(const Cat&) {
+		cout << "복사 생성자" << endl;
+	}
+	Cat& operator=(const Cat&) {
+		cout << "복사 대입연산자" << endl;
+		return *this;
+	}
+	Cat(Cat&&) {
+		cout << "Move 생성자" << endl;
+	}
+	Cat& operator=(Cat&&) {
+		cout << "Move 대입연산자" << endl;
+		return *this;
+	}
+};
+
+template<typename T> void Swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main() {
+	Cat c1;
+	Cat c2;
+	Swap(c1, c2);
+}
