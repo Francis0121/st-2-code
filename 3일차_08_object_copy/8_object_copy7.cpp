cb9cddaf2fda0cd625442c66fd7ef44718ecd0a8
Francis <tjdrms0121@gmail.com>
2015-08-19 17:10:51 +0900

    // C++ 11 은 위개념을 문법화 합니다.  Cat(const Cat&) = delete;// 복사생성자를 지워달라  Cat& operator=(const Cat&) = delete;

--- 3일차_08_object_copy/8_object_copy7.cpp
+++ 3일차_08_object_copy/8_object_copy7.cpp
@@ -10,18 +10,17 @@ public:
 		strcpy(name, n);
 	}
 	~Cat() { delete[] name; }
-	void foo() {
-		Cat c1("A", 2);
-		Cat c2 = c1; // 이것도 막고싶다.
-					// 구현이 없음으로 link 에러!
-	}
-private:
+	
 	// private 복사 생성자 : 복사를 글지하고 싶을떄 사용하는 기술
-	Cat(const Cat& c); // 핵심 .. 선언만 한다.
-	Cat& operator=(const Cat& c);
+	//Cat(const Cat& c); // 핵심 .. 선언만 한다.
+	//Cat& operator=(const Cat& c);
+
+	// C++ 11 은 위개념을 문법화 합니다.
+	Cat(const Cat&) = delete;// 복사생성자를 지워달라
+	Cat& operator=(const Cat&) = delete;
 };
 
 int main() {
 	Cat c1("NABI", 2);
-	//Cat c2 = c1; // 복사를 름지하자.. 컴파일 시간에러가 나오게 해보자.
+	Cat c2 = c1; // 복사를 름지하자.. 컴파일 시간에러가 나오게 해보자.
 }
