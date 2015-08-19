04435dfd2d3a25f818d1c8128ca1f39134cc280c
Francis <tjdrms0121@gmail.com>
2015-08-19 16:03:33 +0900

    // 포인터 멤버를 가진 클래스가 있다면 반드시 복사 생성자를 사용자가 만들어야 한다. // 그렇지 않다면 컴파일러가 만드는 디폴트 생성자는 얕은 복사 현상이 있다..

--- 3일차_08_object_copy/8_object_copy1.cpp
+++ 3일차_08_object_copy/8_object_copy1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// 포인터 멤버를 가진 클래스가 있다면 반드시 복사 생성자를 사용자가 만들어야 한다.
+// 그렇지 않다면 컴파일러가 만드는 디폴트 생성자는 얕은 복사 현상이 있다..
+
 class Cat {
 	char* name;
 	int age;
@@ -10,9 +13,17 @@ public:
 		strcpy(name, n);
 	}
 	~Cat() { delete[] name; }
+	
+	// 자기 자신의 타입 하나를 인자로 가지는 생성자를 
+	// 컴파일러가 만들어줍니다. - 흔히 복사 생성자라고 합니다.
+	Cat(const Cat&c) {
+		name = c.name;
+		age = c.age;
+	}
 };
 
 int main() {
 	Cat c1("NABI", 2);
-	Cat c2 = c1; // runtime error!!
+	// Cat c2 = c1; // runtime error!! 얕은복사(Shallow Copy
+	Cat c2(c1); // 위 한줄은 이 표현과 유사합니다. 
 }
