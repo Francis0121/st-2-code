f2025d2a2636585dedc957290c5e90e46613dc6c
Francis <tjdrms0121@gmail.com>
2015-08-19 17:08:19 +0900

    함수 내부에서도 사용하지 못하도록 막는 방법 선언만 한다.

--- 3일차_08_object_copy/8_object_copy7.cpp
+++ 3일차_08_object_copy/8_object_copy7.cpp
@@ -10,13 +10,18 @@ public:
 		strcpy(name, n);
 	}
 	~Cat() { delete[] name; }
+	void foo() {
+		Cat c1("A", 2);
+		Cat c2 = c1; // 이것도 막고싶다.
+					// 구현이 없음으로 link 에러!
+	}
 private:
 	// private 복사 생성자 : 복사를 글지하고 싶을떄 사용하는 기술
-	Cat(const Cat& c){}
-	Cat& operator=(const Cat& c){}
+	Cat(const Cat& c); // 핵심 .. 선언만 한다.
+	Cat& operator=(const Cat& c);
 };
 
 int main() {
 	Cat c1("NABI", 2);
-	Cat c2 = c1; // 복사를 름지하자.. 컴파일 시간에러가 나오게 해보자.
+	//Cat c2 = c1; // 복사를 름지하자.. 컴파일 시간에러가 나오게 해보자.
 }
