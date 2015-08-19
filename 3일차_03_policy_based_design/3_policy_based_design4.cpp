02c8e2f9f9d46bbdbcd79999ba2203f5035a7441
Francis <tjdrms0121@gmail.com>
2015-08-19 10:50:04 +0900

    단위전략4번째

---
+++ 3일차_03_policy_based_design/3_policy_based_design4.cpp
@@ -0,0 +1,16 @@
+// 3 단위전략 4
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s1 = "ABCD";
+	string s2 = "abcd";
+	
+	if (s1 == s2) {
+		cout << "Same" << endl;
+	}
+	else {
+		cout << "not same" << endl;
+	}
+}
