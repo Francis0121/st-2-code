07b8bec3bcd7e17fa5567a689acafc9548e05be9
Francis <tjdrms0121@gmail.com>
2015-08-19 16:31:19 +0900

    Move 생성자 - rvalue refernece를 사용하기로 결정했다.

--- 3일차_08_object_copy/8_object_copy5.cpp
+++ 3일차_08_object_copy/8_object_copy5.cpp
@@ -11,10 +11,24 @@ public:
 	}
 	~Cat() { delete[] name; }
 
-	Cat(Cat&c) {
+	// C++98 : 생성자, 복사 생성자
+	// C++11 : 생성자, 복사 생성자, Move 생성자
+
+	// 복사 생성자 - 예전 방식과 모양 동일
+	Cat(const Cat& c) { 		
+		// 깊은 복사나 참조계수기법으로 구현.
+		cout << " 복사 생성자" << endl;
+	}
+
+	// Move 생성자 - rvalue refernece를 사용하기로 결정했다.
+	Cat(Cat&& c) {
+		cout << "Move 생성자 호출" << endl;
+		// 소유권 이전으로 구현
+		// 1. 얕은 복사후에
 		name = c.name;
 		age = c.age;
 
+		// 2. 원본을 0으로
 		c.name = 0;
 		c.age = 0;
 	}
