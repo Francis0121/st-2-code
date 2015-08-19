028380b76aedf6c27aa1c692a9e2aaac0947439b
Francis <tjdrms0121@gmail.com>
2015-08-19 12:15:31 +0900

    대부분 성공한다 .. 그런데 .. 너무 위험하고 버그가 많아 진다.

--- 3일차_05_casting/5_casting1.cpp
+++ 3일차_05_casting/5_casting1.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 // C casting의 문제점
+// 대부분 성공한다 .. 그런데 .. 너무 위험하고 버그가 많아 진다.
+// 
 
 int main() {
-	int n = 01;
-	double* p = &n; //?
+//	int n = 01;
+// 	double* p = &n; //? error. 서로 다른 타입의 주소는 암시적 변환이 안됨.
+					// 단, C에서는 가능 ( 확장자 C일경우)
 
-	*p = 3.4;
+//	int n = 01;
+//	double* p = (double*) &n; // 명시적 변환 - ok
+
+//	*p = 3.4;
 
 	const int c = 10;
-	int* p2 = &c; //?
+	//int* p2 = &c; // error. 상수 주소는 비 상수 포인터에 담을 수 없다.
+	int* p2 = (int*)&c;// 명시적 변환 - ok
 	*p2 = 20;
 
 	cout << c << endl;
