b41d7d3d6347c8325aae21b7c55da26c4ab9def4
Francis <tjdrms0121@gmail.com>
2015-08-19 09:29:52 +0900

    thinTemplate

---
+++ 3일차_02_thinTemplate/2_thinTemplate1.cpp
@@ -0,0 +1,32 @@
+// 2_thinTemplate
+
+template<typename T> class Vector {
+
+	T buffer[100];
+	int sz;
+
+public:
+	
+	void push_front(T a){ 
+	
+	}
+	
+	front() { 
+		buffer[0]; 
+	};
+
+	int size() {
+		return sz;
+	}
+
+	bool empty() {
+		return sz == 0;
+	}
+
+};
+
+int main() {
+	Vector<int> v1;
+	Vector<char> v2;
+	Vector<double> v3; // Vector의 멤버 변수는 몇개 일까요?
+}
