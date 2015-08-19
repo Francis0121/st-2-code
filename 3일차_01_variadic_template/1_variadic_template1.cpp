6a90e94cf4268714720a54b1392dd892cde47d3c
Francis <tjdrms0121@gmail.com>
2015-08-19 09:12:12 +0900

    가변 인자 템플릿에서 인자값을 하나씩 알기 위해 하는 기술 (재귀)

---
+++ 3일차_01_variadic_template/1_variadic_template1.cpp
@@ -0,0 +1,30 @@
+// 1_가변인자템플릿
+#include <iostream>
+using namespace std;
+
+// args : 여러개의 인자를 나타낸다. " Parameter Pack"
+
+static int n = 0;
+// 인자 값을 꺼내려면 1번째 인자는 이름이 있는 변수로 받아야 한다.
+template<typename T, typename ... Types> void foo(T value, Types ... args) {
+	
+	// static int n = 0; // 재귀가 호출 되면서 각각 다른 template 이 나오게 된다.
+	// 안에 있는 경우 계속 값이 1이된다.
+	++n;
+	cout << n << " ";
+
+	cout << typeid(T).name() << " : " << value << endl;
+
+	foo(args...); // args : 3.3, 'a', "Hello"
+				  // args : 'a', "Hello"
+				  // args : "Hello"
+				  // args : 
+}
+
+void foo() {
+
+}
+
+int main() {
+	foo(1, 3.3, 'a', "hello"); // value : 1		args : 3.3 , 'a', "hello"
+}
