611ca5c24d0668038b959d9fee5953672cdc509f
Francis <tjdrms0121@gmail.com>
2015-08-19 16:48:47 +0900

    // C++11 이 전 -> 소멸자, 복사 생성자, 복사 대입연산자 -> Rule of 3  // C++11 이 후 -> Move 생성자, Move 대입연산자 -> Rule of 5

--- 3일차_08_object_copy/8_object_copy6.cpp
+++ 3일차_08_object_copy/8_object_copy6.cpp
@@ -7,6 +7,8 @@ public:
 	Cat() {
 		cout << "생성자" << endl;
 	}
+	// C++11 이 전 -> 소멸자, 복사 생성자, 복사 대입연산자 -> Rule of 3
+	// C++11 이 후 -> Move 생성자, Move 대입연산자 -> Rule of 5
 	~Cat() {
 		cout << "소멸자" << endl;
 	}
