88385f9e55f416afceff80f8ebfff300ee8f325d
Francis <tjdrms0121@gmail.com>
2015-08-19 12:19:43 +0900

    static_cast

---
+++ 3일차_05_casting/5_casting2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// C++은 4개의 캐스팅 연산자를 지원한다.
+// static_cast : void* => 다른 타입은 허용
+//				 그 외에는 연광성이 있어야 한다.
+//
+// reinterpret_cast :
+// const_cast :
+// dynamic_cast :
+
+int main() {
+
+	int* p3 = static_cast<int*>( malloc(100) ); // void * -> int *  변환
+
+	int n = 01;
+	double* p = (double *) &n; 
+	
+	const int c = 10;
+	int* p2 = (int*)&c;
+	
+}
