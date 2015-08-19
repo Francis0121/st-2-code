91a71e9302c9a2741199c6f0246d152e589877bb
Francis <tjdrms0121@gmail.com>
2015-08-19 10:08:28 +0900

    부분 전문화 타입 알아내기

--- 3일차_01_variadic_template/1_variadic_template3.cpp
+++ 3일차_01_variadic_template/1_variadic_template3.cpp
@@ -39,8 +39,18 @@ template<typename T> int xtuple_size(T& a) {
 	return T::N;
 }
 
+// xtuple의 가진 N번째 요소의 타입을 알아내는 기술 - 어렵습니다.
+template<int N, typename T, typename ... Types>
+struct xtuple_type{
+
+};
+
+// 0 번째 타입을 요구할때를 위한 부분 전문화
+template<int N, typename T, typename ... Types>
+struct xtuple_type<0, x_tuple<T, Types...>> {
+
+};
+
 int main() {
 	xtuple<int, char, double, short> t4(1, 'c', 3.3, 4);
-	cout << xtuple_size(t4) << endl;
-	xtuple<int> t1(3);
 }
