b6e1c117e53c3c2d8b046e3c2967e179925e006a
Francis <tjdrms0121@gmail.com>
2015-08-19 12:23:02 +0900

    reinterpret_cast : 메모리의 재해석, 대부분 성공.

--- 3일차_05_casting/5_casting2.cpp
+++ 3일차_05_casting/5_casting2.cpp
@@ -5,17 +5,20 @@ using namespace std;
 // static_cast : void* => 다른 타입은 허용
 //				 그 외에는 연광성이 있어야 한다.
 //
-// reinterpret_cast :
+// reinterpret_cast : 메모리의 재해석, 대부분 성공.
+// 
 // const_cast :
 // dynamic_cast :
 
 int main() {
 
-	int* p3 = static_cast<int*>( malloc(100) ); // void * -> int *  변환
+	int* p3 = static_cast<int*>( malloc(100) ); // void* -> int*  변환
 
 	int n = 01;
-	double* p = (double *) &n; 
-	
+	// double* p = (double *) &n; 
+	// double* p = static_cast<double*>(&n);// int* -> double*
+										// error
+	double* p = reinterpret_cast<double*>(&n);
 	const int c = 10;
 	int* p2 = (int*)&c;
 	
