bfd5ea69a8449b0e2711a8faf6cef1d87efa9e2d
Francis <tjdrms0121@gmail.com>
2015-08-20 11:11:12 +0900

    스마트 포인터 정리!

--- 4일차_02_smart_pointer/2_smart_pointer2.cpp
+++ 4일차_02_smart_pointer/2_smart_pointer2.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// 1. Template으로 만들어야한다.
+
+// 2. 얕은복사 문제를 해결해야 한다.
+//		A. 깊은복사 - 스마트 포인터 만들때는 거의 사용하지 않는다.
+//		B. 참조계수 - 스마트 포인터 만들때 "가장" 널리 쓰이는 방법
+//		C. 소유권이전 - ???
+//		D. 복사금지 - 요즘에 뜨고 있는 방식. ( 최 신 유 행 )
+
 template <typename T>
 class Ptr {
 	T* obj;
@@ -20,3 +28,8 @@ public:
 		return *obj;
 	}
 };
+
+int main() {
+	Ptr<int> p1 = new int;
+	Ptr<int> p2 = p1;
+}
