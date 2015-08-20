af4f2cf2d2b9e76702674ff3ef364e39a773188b
Francis <tjdrms0121@gmail.com>
2015-08-20 09:08:38 +0900

    operator int 변환 연산자 함수 : 객체를 다른 타입으로 암시적 형변환 되도록 한다

--- 4일차_01_t​ransformation/1_t​ransformation1.cpp
+++ 4일차_01_t​ransformation/1_t​ransformation1.cpp
@@ -11,12 +11,16 @@ public :
 	Point(int a, int b) : x(a), y(b){
 	
 	}
+
+	// 변환 연산자 함수 : 객체를 다른 타입으로 암시적 형변환 되도록 한다.
+	// 특징 : 함수 이름에 리턴타입이 들어 있음으로 리턴 타입을 표시하지 않는다.
+	operator int() { return x; }
 };
 
 int main() {
 	double d = 3.4;
-	int n = d;
+	int n = d; // Primitive Type 은 서로 암시적 형 변환이 가능하다.
 
 	Point p(1, 2);
-	n = p;
+	n = p; // p.operator int() 함수가 있으면 된다. -> operator int() : 변환연산자
 }
