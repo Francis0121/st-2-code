0f3fb41e1280cda006cf2e02764f347635603d91
Francis <tjdrms0121@gmail.com>
2015-08-20 11:36:08 +0900

    스마트 포인터 만들기

--- 4일차_02_smart_pointer/2_smart_pointer5.cpp
+++ 4일차_02_smart_pointer/2_smart_pointer5.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+#include <memory>
+using namespace std;
+
+// 이미 C++ 표준에는 참조계수 스마트 포인터가 있습니다.
+int main() {
+	shared_ptr<int> p1(new int);
+	shared_ptr<int> p2 = p1; // 이 순간 참조계수가 증가합니다.
+}
