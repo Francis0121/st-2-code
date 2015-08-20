8fae0a723ada4e9ebb81263a0f2d60e17df8e678
Francis <tjdrms0121@gmail.com>
2015-08-20 11:25:25 +0900

    스마트 포인터에는 있어야 할것이 참 많다.

--- 4일차_02_smart_pointer/2_smart_pointer4.cpp
+++ 4일차_02_smart_pointer/2_smart_pointer4.cpp
@@ -14,14 +14,24 @@ class Ptr {
 	T* obj;
 	int* ref;
 public:
-	Ptr(T* p = 0) : obj(p) {
+	// 변환생성자를 막기 위한 방법으로 다음을 수행
+	explicit Ptr(T* p = 0) : obj(p) {
 		ref = new int(1);
 	}
+	// 일반 복사 생성자
 	Ptr(const Ptr& p) {
 		obj = p.obj;
 		ref = p.ref;
 		++(*ref);
 	}
+	// 일반화된 복사 생성자 - 2일차 2교시 참고.
+	template<typename U> Ptr(const Ptr<U>& p) {
+		obj = p.obj;
+		ref = p.ref;
+		++(*ref);
+	}
+
+	template<typename> friend class Ptr;
 
 	~Ptr() {
 		if (--(*ref) == 0) {
@@ -37,9 +47,11 @@ public:
 	T& operator*() {
 		return *obj;
 	}
+
+	// 그외 대입연산자(=), ==, != 연산자가 template 버전으로 있어야 한다.
 };
 
 int main() {
-	Ptr<Dog> p1 = new Dog;
-	Ptr<Animal> p2 = p1; 
+	Ptr<Dog> p1(new Dog);
+	Ptr<Animal> p2 = p1; // 다형성 : 부모 포인터로 자식을 가리킬 수 있어야 한다.
 }
