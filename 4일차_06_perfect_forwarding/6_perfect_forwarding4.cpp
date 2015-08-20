93357d4ea5de8f9e04dba59906c72beb516662c2
Francis <tjdrms0121@gmail.com>
2015-08-20 16:45:03 +0900

    my_referencet wrappe의 이용

--- 4일차_06_perfect_forwarding/6_perfect_forwarding4.cpp
+++ 4일차_06_perfect_forwarding/6_perfect_forwarding4.cpp
@@ -14,14 +14,17 @@ void foo(int a) { cout << "foo" << endl; }
 void goo(int& a) { cout << "goo" << endl; a = 20; }
 
 template<typename F, typename T>
-void HowLong(F f, const T& a) {
+void HowLong(F f, T a) {
 	f(a);
 }
 
 int main() {
 	int n = 0;
 	HowLong(foo, 0);
-	HowLong(goo, n);
+	//HowLong(goo, n);
+	// n을 참조로 보내고 싶다면 이동 가능한 참조를 사용한다.
+	my_reference_wrapper<int> r1 = n;
+	HowLong(goo, r1);
 
 	cout << n << endl;
 }
