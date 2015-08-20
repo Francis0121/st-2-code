f6e6df5142ba6d4bdeb932ca5cb460ebfe923bbd
Francis <tjdrms0121@gmail.com>
2015-08-20 15:55:58 +0900

    복사 금지 스마트 포인터 끝?

--- 4일차_05_checked_delete/5_checked_delete.cpp
+++ 4일차_05_checked_delete/5_checked_delete.cpp
@@ -6,7 +6,10 @@ class Test; // 클래스 전방선언 - 완전한 선언이 없이도 포인터 변수를 사용할 수 있
 Test* p; // incomplete objecct(불완전한 객체)
 		// 전방선언만 있는 타입의 포인터 delete 하면 소멸자를 호출하지 못한다.
 
+// ~ 죽음
+// checked delete - boost 팀이 최초로 만든 개념.
 void foo(Test* pt) {
+	enum { type_must_be_complete = sizeof(Test) }; // 불완전 객체의 크기를 구할 수 없다.
 	delete pt;
 }
 
@@ -19,6 +22,12 @@ public:
 		cout << "소멸자" << endl;
 	}
 };
+
+// ~ 컴파일되고 실행됨
+//void foo(Test* pt) {
+//	sizeof(Test); // 불완전 객체의 크기를 구할 수 없다.
+//	delete pt;
+//}
 
 int main() {
 	Test* p = new Test;
