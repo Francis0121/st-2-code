b16a447909e93791576319483d185943a494eadd
Francis <tjdrms0121@gmail.com>
2015-08-20 12:42:30 +0900

    이제는모르겠다.

--- 4일차_02_smart_pointer/2_smart_pointer12.cpp
+++ 4일차_02_smart_pointer/2_smart_pointer12.cpp
@@ -25,20 +25,27 @@ public:
 	}
 };
 
+// 참조계수를 조작하는 함수는 반드시 상수 함수 이어야 한다.
 template<typename T>
 class LightRefBase {
-	int mCount;
+	mutable int mCount;
 public:
 	inline LightRefBase() : mCount(0) {
 	}
 	inline ~LightRefBase() {
 	}
-	inline void incStrong() {
+	inline void incStrong() const {
 		++mCount;
 	}
-	inline void decStrong() {
+	inline void decStrong() const {//decStrong (const LightRefBase* this)
+		// this : const RefBase* 입니다.
+		// static cast는 상수성을 제거할 수 없습니다.
+
+		// RefBase*			=> T*		: ok!!
+		// const RefBase*	=> const T*	: ok!!
+		// const RefBase*	=> T*		:error
 		if (--mCount == 0) {
-			delete static_cast<T*>(this);
+			delete static_cast<const T*>(this);
 		}
 	}
 };
@@ -52,4 +59,6 @@ public:
 
 int main() {
 	sp<Truck> p = new Truck;
+	//const Truck t;
+	//t.incStrong(); // 상수 객체도 참조 계수 관리는 할 수 있어야 한다.
 }
