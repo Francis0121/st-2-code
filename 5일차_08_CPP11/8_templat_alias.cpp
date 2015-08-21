d05b4d476b4af2a2ee971483b8e619dcf58ef8f5
Francis <tjdrms0121@gmail.com>
2015-08-21 15:37:46 +0900

    #include <vector> using namespace std; template <typename T> class MyAlloc{};
    // C++ 11의 using은 template이 된다.
    template<typename T>
    using MyVector = vector<T, MyAlloc<T>>;

--- 5일차_08_CPP11/8_templat_alias.cpp
+++ 5일차_08_CPP11/8_templat_alias.cpp
@@ -3,6 +3,18 @@
 // 복잡한 타입은 typedef 먼저 만드는 것이 좋다.
 //typedef void(*PF)();
 
+// typedef vector<int, MyAlloc<int>> MyVector;
+// 아래 코드처럼 사용하고 싶을 때가 많다.
+// template<typename T> typedef vector<T, MyAlloc<T>> MyVector; // 안됨
+
+#include <vector>
+using namespace std;
+template <typename T>
+class MyAlloc{};
+// C++ 11의 using은 template이 된다.
+template<typename T>
+using MyVector = vector<T, MyAlloc<T>>;
+
 // C++ 11
 using PF = void(*)();
 
@@ -10,4 +22,6 @@ int main(){
 
 	// void(*f)();
 	PF f;
+
+	//MyVector<int> v;
 }
