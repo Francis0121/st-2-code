ec992a0318a83557079e677427b6fd3889321692
Francis <tjdrms0121@gmail.com>
2015-08-21 09:13:22 +0900

    간접층의 원리 정리.

--- 5일차_01_간접층의원리/1_간접층의원리1.cpp
+++ 5일차_01_간접층의원리/1_간접층의원리1.cpp
@@ -1,18 +1,29 @@
 // 1 간접층의 원리
 
+// 아무리 어려운 S/W 문제점도 중간층(기존 요소 사이에 새로운 요소)를 도입하면 해결할 수 있다.
+// Level of indirection 이라는 원리
+
+struct VectorSize {
+	int size;
+	VectorSize(int n)  // int => VectorSize로 암시적 변환된다.
+		: size(n) { }
+};
+
 class Vector {
 public:
-	Vector(int sz) {
+	// 생성자 인자가 하나있다면 explicit가 있는것이 좋다.
+	/*explicit Vector(int sz) {
 
+	}*/
+	Vector(VectorSize sz) { // VectorSize => Vector로 암시적 변환된다.
+		int size = sz.size;
 	}
 };
 
-void foo(Vector v) {
-
-}
+void foo(Vector v) { }
 
 int main() {
-	Vector v(10);
-	foo(v);
-	foo(20); // ??
+	Vector v(10); 
+	foo(v); // ok
+	foo(20); // int => Vector 즉, 변환생성자가 있으면 ok
 }
