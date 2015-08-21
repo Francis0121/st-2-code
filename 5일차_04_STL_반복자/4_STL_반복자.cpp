f74c6db569e489453273e336a53085c45c682b4b
Francis <tjdrms0121@gmail.com>
2015-08-21 10:53:02 +0900

    iterator

--- 5일차_04_STL_반복자/4_STL_반복자.cpp
+++ 5일차_04_STL_반복자/4_STL_반복자.cpp
@@ -11,24 +11,27 @@ template <typename T> struct Node {
 
 // list 안에 있는 Node를 가르키는 스마트 포인터를 도입하자
 // ++로 이동하고 *로 값을 꺼낼 수 있게 하자
+
+// 반복자 : 컨테이너의 요소를 차례대로 이동하기 위한 객체
+//		    스마트 포인터와 유사하지만 자원 해지가 아닌 요소의 열거
 template<typename T> class slist_iterator {
 	Node<T>* current;
 public:
-	slist_iterator(Node<T>* p = 0) : current(p){}
+	inline slist_iterator(Node<T>* p = 0) : current(p){}
 
 	// xfind로 보내려면 진짜 포인터처럼 동작해야 한다.
 	// ++, *, ==, != 있어야 한다.
 	inline slist_iterator& operator++() {
 		current = current->next;
-		return *this
+		return *this;
 	}
 	inline T& operator*() {
 		return current->data ; 
 	}
-	inline  bool operator==(const slist_iterator& t){
+	inline bool operator==(const slist_iterator& t){
 		return current == t.current;
 	}
-	inline bool operator!=(cinst slist_iterator& t) {
+	inline bool operator!=(const slist_iterator& t) {
 		return current != t.current;
 	}
 };
@@ -42,6 +45,23 @@ public:
 	void push_front(const T& a) {
 		head = new Node<T>(a, head);
 	}
+
+	// 모든 컨테이너 설계자는 자신의 반복자 이름을
+	// 미리 약속된 interator 라는 이름을 외부에 알려야 한다.
+	typedef slist_iterator<T> iterator;
+	
+	// 모든 컨테이너는 자신의 시작요소와 마지막 다음요소를 가르키는 반복자를 리턴하는 함수를 제공해야 한다,
+	// 반복자를 리턴하는 함수를 제공해야한다.
+
+	iterator begin() {
+		return iterator(head);
+	}
+
+	iterator end() {
+		return iterator(0);
+	}
+
+
 };
 
 int main() {
@@ -49,4 +69,11 @@ int main() {
 	s.push_front(10);
 	s.push_front(20);
 	s.push_front(30);
+
+	iterator p = s.begin();
+	// 이제부터는 p를 s의 c의 처음 요소를 가르키는포인터로 생각하면 됩니ㅏㄷ.
+	while (p != s.end()) {
+		cout << *p << endl;
+		++p;
+	}
 }
