8abebd4d482099c5bb01366bfdbe99b6a1799d1c
Francis <tjdrms0121@gmail.com>
2015-08-21 16:27:46 +0900

    0i 31

---
+++ 5일차_08_CPP11/8_사용자정의상수2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <complex>
+using namespace std;
+
+void foo(const char* s) { cout << "char*" << endl; }
+void foo(string s) { cout << "string" << endl; }
+
+int main() {
+	foo("hello");			// 1
+	foo(string("hello"));	// 2
+	foo("hello"s);
+
+	complex<int> c1(3); // 3 + 0i
+	cout << c1 << endl;
+
+	complex<int> c2(0, 3); // 0 + 3i
+	cout << c2 << endl;
+
+	complex<int> c3(3i);
+	cout << c3 << endl;
+}
