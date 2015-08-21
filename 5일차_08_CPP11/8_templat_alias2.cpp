27281eb3d137cd66b91cfbc9d6809d1a7e9bab7e
Francis <tjdrms0121@gmail.com>
2015-08-21 15:46:42 +0900

    make remove_const_t

--- 5일차_08_CPP11/8_templat_alias2.cpp
+++ 5일차_08_CPP11/8_templat_alias2.cpp
@@ -8,9 +8,20 @@ struct xremove_const {
 	typedef T type;
 };
 
+template<typename T>
+struct xremove_const<const T> {
+	typedef T type;
+};
+
+// 위처럼 traits를 만들어 놓고
+// using으로 사용ㄹ하기 쉽게 해주면 됩니다.
+template <typename T>
+using xremove_const_t = typename xremove_const<T>::type;
+
 template<typename T> void foo(T& a) {
 	// T에서 const를 제거한 타입을 구하는 방법
-	xremove_const<T>::type n;
+ 	// typename xremove_const<T>::type n;
+	xremove_const_t<T> n; // 위와 동일한 표현이다.
 }
 
 int main() {
