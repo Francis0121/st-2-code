cbf5427d8e39efa8847b88d41e75ad635df9cf6f
Francis <tjdrms0121@gmail.com>
2015-08-21 10:17:55 +0900

    Step4

--- 5일차_02_STL_알고리즘/2_STL_알고리즘3.cpp
+++ 5일차_02_STL_알고리즘/2_STL_알고리즘3.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Step 3. 검색 대상 타입의 일반화 - 템플릿 도입.
 
+// 모든 타입의 배열 [first, last) 사이에서 선형 검색을 수행하는 함수.
 template<typename T>
 T* xfind(T* first, T* last, T c) {
 	while (first != last && *first != c)
---
+++ 5일차_02_STL_알고리즘/2_STL_알고리즘4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Step 4. 보다 일반화 되게 ...
+
+// 아래 처럼 만들면 2개의 문제가 있습니다.
+// 1. 구간의 타입과 찾는 요소의 타입이 연관됩니다. 
+//	  double 배열에서 int를 찾을 수 없습니다.
+
+// 2. T* 라고 하면 진짜 포인터만 사용가능합니다.
+//	  스마트 포인터를 사요할 수 없습니다.
+template<typename T>
+T* xfind(T* first, T* last, T c) {
+	while (first != last && *first != c)
+		++first;
+
+	return first == last ? 0 : first;
+}
+
+int main() {
+	double x[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// double* p = xfind(x, x + 10, 5); 이거안됨 double dobule int 라서
+	double* p = xfind(x, x + 10, 5.0);
+
+	cout << *p << endl;
+}
