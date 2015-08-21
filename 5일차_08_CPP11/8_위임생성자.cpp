3208159acd33ed15d23b23bd8aaeaf4b91fa92c4
Francis <tjdrms0121@gmail.com>
2015-08-21 14:59:07 +0900

    delegating constructor

---
+++ 5일차_08_CPP11/8_위임생성자.cpp
@@ -0,0 +1,16 @@
+// delegating constructor
+#include <iostream>
+using namespace std;
+class Point {
+	int x, y;
+public:
+	Point(int a, int b) : x(a), y(b){}
+	
+	Point() {
+
+	}
+};
+
+int main() {
+
+}
