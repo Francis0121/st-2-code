75a09a7388c4be2aa13e5e16e43c13411f03c1a6
Francis <tjdrms0121@gmail.com>
2015-08-21 14:16:37 +0900

    initializer_list<?>

--- 5일차_08_CPP11/8_InitializerList.cpp
+++ 5일차_08_CPP11/8_InitializerList.cpp
@@ -2,7 +2,18 @@
 #include <iostream>
 using namespace std;
 
+void foo(initializer_list<int> e) {
+	auto p = e.begin(); // begin(e)
+
+	while (p != e.end()) { // end(e)
+		cout << *p << endl;
+		++p;
+	}
+}
+
 int main() {
 	initializer_list<int> e1{ 1,2,3,4,5 };
 	initializer_list<int> e2 = { 1,2,3,4,5 };
+
+	foo(e1);
 }
