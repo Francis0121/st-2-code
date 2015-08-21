e5527e9233a1d3f0f4ee9041c26133cd9d0fafbd
Francis <tjdrms0121@gmail.com>
2015-08-21 15:03:06 +0900

    위임 생성자 자바에서 this(0, 0);

--- 5일차_08_CPP11/8_위임생성자.cpp
+++ 5일차_08_CPP11/8_위임생성자.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 using namespace std;
 class Point {
-	int x, y;
+	
 public:
+	int x, y;
+
 	Point(int a, int b) : x(a), y(b){}
 	
-	Point() {
-
+	Point() : Point(0, 0) { // 생성자에서 다른 생성자를 호출하는 코드
+		// Point(0, 0); //생성자에서 다른 생성자 호출이 아니고 임시객체를 만드는 표현이다.
 	}
 };
 
 int main() {
-
+	Point p;
+	cout << p.x << endl;
 }
