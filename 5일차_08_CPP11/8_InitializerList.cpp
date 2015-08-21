dbe9ae26e64c9d3b4ad1d97242c1973149daa088
Francis <tjdrms0121@gmail.com>
2015-08-21 14:14:13 +0900

    CPP11 2. ~ initializer_list

---
+++ 5일차_08_CPP11/8_InitializerList.cpp
@@ -0,0 +1,8 @@
+// InitializerList
+#include <iostream>
+using namespace std;
+
+int main() {
+	initializer_list<int> e1{ 1,2,3,4,5 };
+	initializer_list<int> e2 = { 1,2,3,4,5 };
+}
--- 5일차_08_CPP11/8_일관된초기화1.cpp
+++ 5일차_08_CPP11/8_일관된초기화1.cpp
@@ -27,5 +27,8 @@ int main() {
 	Complex c{ 0, 0 };
 
 	int n3 = 3.4; // ok ... C와의 호환성 때문에 C++도 지원하기로 결정.
-	int n4{ 3.4 };
+//	int n4{ 3.4 }; // error . preventing narrow
+
+//	char c1{ 300 }; // error. 300은 1byte가 표현될 수 없다.
+	unsigned char c2{ 200 }; // ok..
 }
