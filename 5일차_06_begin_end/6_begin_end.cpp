cddb251f2d3d97fb319cc2b7e8fa67d610db4ff8
Francis <tjdrms0121@gmail.com>
2015-08-21 12:20:23 +0900

    begin_end

--- 5일차_06_begin_end/6_begin_end.cpp
+++ 5일차_06_begin_end/6_begin_end.cpp
@@ -4,10 +4,37 @@
 #include <type_traits>
 using namespace std;
 
+// 컨테이너의 모든 요소를 화면에 출력하는 함수.
+template<typename T> void showImpl(T& c, false_type) {
+	typename T::iterator p = c.begin();
+
+	while (p != c.end()) {
+		cout << *p << " ";
+		++p;
+	}
+	cout << endl;
+}
+
+// 배열일때
+template<typename T> void showImpl(T& c, true_type) {
+	auto p = c; // 배열이 이름은 시작 주소이다.
+ 
+	while (p != c + extent<T,0>::value) {
+		cout << *p << " ";
+		++p;
+	}
+	cout << endl;
+}
+
+template<typename T> void show(T& c) {
+	showImp(c, ? );
+}
+
 int main() {
 	int x[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	
 	vector<int> v(x, x + 10);
 
-	show();
+	show(v);
+	show(x);
 }
